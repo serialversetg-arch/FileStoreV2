3c518345ce6abc959d74610fe53588f2b71d9888
serialversetg-arch <serialversetg@gmail.com>
2026-03-04 11:02:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import os
 
 class Config(object):
     # --- API & Tokens ---
-    API_ID = int(os.environ.get("API_ID", "12345"))
-    API_HASH = os.environ.get("API_HASH", "your_api_hash")
+    API_ID = int(os.environ.get("API_ID", "26910777"))
+    API_HASH = os.environ.get("API_HASH", "8601f2f24993f6fdbcbac3bb27ceec38")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
     
     # --- Database (MongoDB) ---
-    DB_URL = os.environ.get("DB_URL", "your_mongodb_url")
-    OWNER_ID = int(os.environ.get("OWNER_ID", "12345678"))
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://King:Cobra765592@cluster0.qy4m5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5232142502"))
     
     # --- Channels (Must be Integers) ---
     FSUB_CHANNEL = int(os.environ.get("FSUB_CHANNEL", "-1002730333831")) 
-    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100...")) 
+    DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002605527708")) 
     FSUB_ON = True 
 
     # --- Links ---
